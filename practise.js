37dfbe82abe9596cfc835534f7c02cc4e206449a
AlkaTiwari2002 <alkat5359@gmail.com>
2024-03-24 23:56:11 +0530

    first

---
+++ practise.js
@@ -0,0 +1,87 @@
+// let a=25;                                              Q=1
+// let b=5;
+// let sum = a+b;
+// let difference = a-b;
+// let product = a*b;
+// let quotient = a/b;
+// let modulus = a%b;
+// console.log(sum,difference,product,quotient,modulus);
+
+// let a=99;                                                          Q=2
+// let b=5;
+// console.log(parseInt(a/b));
+
+// let c=5;                                                        Q=3
+// let d=2;
+// console.log(c**d);
+
+// let a=25;
+// let b=4;
+// let c=a/b;
+// const decimalPart = c - Math.floor(c);
+// console.log(decimalPart);
+
+// let a=5;                                                       Q=5
+// let b=4;
+// let c=6;
+// let d=8;
+// let e=2;
+// let f=(a+b+c+d+e)/5;
+// console.log(f);
+
+// let SP=500;                                                               Q=6
+// let CP=400;
+// let profit = SP-CP;
+// console.log(profit);
+
+// let a=4536;                                                                Q=7
+// let last=a%10;
+// console.log(last);
+
+// let a=99;                                                        Q=8
+// let first=parseInt(a/10);
+// let last=a%10;
+// console.log(first,last);
+
+// let a=9;                                                              Q=9
+// let b=12;
+// let c=5;
+// if (a>b && a>c){
+//     console.log(a)
+// }
+// else if (b>a && b>c){
+//     console.log(b)
+// }
+// else{
+//     console.log(c)
+// }
+
+// let a=24;                                                               Q=10
+// if (a%2===0){
+//     console.log("Even")
+// }
+// else{
+//     console.log("Odd")
+// }
+
+
+// let a=11;                                                                    Q=11
+// let i=1,count=0;
+// while(i<=a){
+//     if(a%i==0){
+//         count++;
+//     }i++;
+// }
+// if(count==2){
+//     console.log("this is prime number");
+// }else{
+//     console.log("this is not prime number");
+// }
+
+// let cel=10;                             Q=12
+// let f=(cel*9/5)+32;
+// console.log(f);
+// f=50;
+// cel=(f-32)*5/9;
+// console.log(cel);
+
